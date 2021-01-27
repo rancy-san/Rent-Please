d4afd74593b71cd9edaa99b3d36ae1959c213694
rancy-san <san00003@algonquinlive.com>
2021-01-26 20:56:28 -0800

    Updated class comments

--- src/ts/rentPlease.ts
+++ src/ts/rentPlease.ts
@@ -1,13 +1,33 @@
+
+/**
+ * Filename:        rentPlease.ts
+ * Description:     This file contains tasks to call functions to
+ *                  process rental data managed by other classes.
+ * 
+ */
+
+ // Web crawling class
 const CrawlRental = require('./crawlRental');
 
 export class RentPlease {
-
+    // holds the crawlRental class to webcrawl rental web applications
     private crawlRental:any;
 
+    /**
+     * Constructor description:     Instantiate classes for use in the Rent, Please! application.
+     * Parameter:                   arg:string, the processed argument character from the commandline.
+     */
     constructor(arg:string){
+        // instantiate web crawler to first obtain rental data pointing to the arg parameter
        this.crawlRental = new CrawlRental(arg);
     }
 
+    /**
+     * Function Name:   seekRental
+     * Description:     Crawl the web application indicated by the parameter
+     *                  set during initializtion of the CrawlRental object 
+     *                  in the constructor.
+     */
     public seekRental():void {
         this.crawlRental.crawl();
     }
