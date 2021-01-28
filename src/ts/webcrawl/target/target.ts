266559a879efda91012db9a4c34ef0d1f634b91d
Rancy <70251413+rancy-san@users.noreply.github.com>
2021-01-27 20:56:31 -0800

    Updated abstract class to include extra data parameter

--- src/ts/webcrawl/target/target.ts
+++ src/ts/webcrawl/target/target.ts
@@ -3,14 +3,16 @@ abstract class Target {
     private browser:any;
     private targetURL:string;
     private rentalType:object;
+    private districtList:object;
 
-    constructor(browser:any, targetURL:string, rentalType:object){
+    constructor(browser:any, targetURL:string, rentalType:object, districtList:object){
         this.browser = browser;
         this.targetURL = targetURL;
         this.rentalType = rentalType;
+        this.districtList = districtList;
     }
 
-    public async search(any){};
+    public async search(){};
 
 }
 
