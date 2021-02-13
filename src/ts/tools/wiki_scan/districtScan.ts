57421f0809ed5f9aafddc257a8d95c80fca81e46
rancy-san <san00003@algonquinlive.com>
2021-02-13 00:01:50 -0800

    Removed obsolete code, added comments for districtScan, added correct scan for districts

--- src/ts/tools/wiki_scan/districtScan.ts
+++ src/ts/tools/wiki_scan/districtScan.ts
@@ -1,19 +1,47 @@
+
+/**
+ * Filename:        districtScan.ts
+ * Description:     This file scans for data from the Wikipedia site for
+ *                  some districts that exist for provinces and cities.
+ *                  Saving the data is also performed.
+ * 
+ */
+// Wikipedia library
 const Wikipedia = require('node-wikipedia');
+// list of province names and codes
 const ProvinceList = require('../../../json/provinceList');
 
 class DistrictScan {
+    // list of district information collected from Wikipedia
     private district: object
+    // province name from the province list
     private provinceName: string[];
+    // the JSON key for the province
     private provinceNameKey: string;
+    // the JSON key for the province code
     private provinceCodeKey: string;
+    // JSON key for selecting the subcategory in the Wikipedia response
     private subcategoryKey: string;
+    // JSON key for selecting the page of a subcategory in the Wikipedia response
     private subcategoryPageKey: string;
+    // JSON key for selecting the subcategory's name in the Wikipedia response
     private subcategoryNameKey: string;
-    private searchTerm: string;
+    // primary filter for searching
+    private searchTermPrimary: string;
+    // secondary filter for parsing
+    private searchTermSecondary: string[];
 
+    /**
+     * Description:     Storing initial data for the purpose of scanning for districts names
+     */
     constructor() {
         this.district = {};
-        this.searchTerm = "Neighbourhoods in ";
+        this.searchTermPrimary = "Neighbourhoods in ";
+        this.searchTermSecondary = [
+            this.searchTermPrimary,
+            "Communities in ",
+            "Communities in the "
+        ];
         this.provinceNameKey = "province_name";
         this.provinceCodeKey = "province_code";
         this.subcategoryKey = "subcategories";
@@ -22,15 +50,11 @@ class DistrictScan {
         this.provinceName = ProvinceList[this.provinceNameKey];
     }
 
-    private async searchPages(districtName: string) {
-        return Wikipedia.page.data(
-            districtName,
-            { content: true },
-            function (response) {
-                return response;
-            });
-    }
-
+    /**
+     * Function Name:   searchCategories
+     * Description:     Perform a Wikipedia search for the district
+     * @param           {string} districtName the name of a district, town or province
+     */
     private async searchCategories(districtName: string) {
         let category;
         let p = new Promise<void>((resolve, reject) => {
@@ -47,24 +71,32 @@ class DistrictScan {
         });
     }
 
-    public async search() {
-        await this.searchByProvince();
-    }
-
+    /**
+     * Function Name:   searchByProvince
+     * Description:     Perform a search using province names in the search term
+     */
     private async searchByProvince() {
         // get province length to scan districts
         let provinceNameLength: number = this.provinceName.length;
         // cycle through each province
         while (provinceNameLength--) {
             // get the list of categories for the province
-            let category: object = await this.searchCategories(this.searchTerm + this.provinceName[provinceNameLength]);
+            let category: object = await this.searchCategories(this.searchTermPrimary + this.provinceName[provinceNameLength]);
             // initialize each province with empty object
             this.district[this.provinceName[provinceNameLength]] = {};
+            // any results will be a subcategory leading to more subcategories or pages of districts
             this.searchBySubcategory(category);
         }
     }
 
-    private async searchBySubcategory(category) {
+    /**
+     * Function Name:   searchBySubcategory
+     * Description:     Recursive function performs deeper search of subcateogries within 
+     *                  subcatogeries until a page for a district is found.
+     * @param           {object} category the JSON data for the category/subcategory 
+     *                  used to perform a deeper search 
+     */
+    private async searchBySubcategory(category: object) {
         //console.log(category);
         // array of objects from the wiki response
         let subcategory: object[] = category[this.subcategoryKey];
@@ -74,9 +106,10 @@ class DistrictScan {
         let subcategoryLength: number;
 
         try {
-            //console.log(subcategory);
+            // length may not exist if the subcategory is missing
             subcategoryLength = subcategory.length;
         } catch {
+            // prevent future looping
             subcategoryLength = 0;
         }
 
@@ -84,25 +117,72 @@ class DistrictScan {
         if (subcategoryLength > 0) {
             // scan through subCategories
             while (subcategoryLength--) {
+                // recursive loop to perform another category search within the JSON data structure
                 await this.searchBySubcategory(subcategory[subcategoryLength]);
             }
-        } else {
-            console.log(subcategoryName);
-            let page: string[] = category[this.subcategoryPageKey];
-            let pageLength: number;
-
-            try {
-                pageLength = page.length;
-            } catch {
-                pageLength = 0;
-            }
-
-            if (pageLength > 0) {
-                while (pageLength--) {
-                    //console.log(page[pageLength]);
+        }
+        // search through existing pages that are not dependent of the existance of a recursive loop
+        if (this.getPageLength(this.getPage(category)) > 0) {
+            // determine number of filters
+            let searchTermSecondaryLength = this.searchTermSecondary.length;
+            // cycle through filters
+            while (searchTermSecondaryLength--) {
+                // accept pages only if the subcategory contains the correct text (ignore non-district data)
+                if (subcategoryName.indexOf(this.searchTermSecondary[searchTermSecondaryLength]) >= 0) {
+                    // enter the page JSON key
+                    this.searchByPage(category);
                 }
             }
         }
+    }
+
+    /**
+     * Function Name:   searchByPage
+     * Description:     Scan through the page/s avaiable by using the category that the pages belong in.
+     *                  Pages with acceptable filter will be considered.
+     * @param           {object} category the JSON data for the category/subcategory to access the page data
+     *                  used to perform a deeper search 
+     */
+    private async searchByPage(category: object) {
+        // page data from the subcategory/category
+        let page: string[] = this.getPage(category);
+        // number of pages
+        let pageLength: number = page.length;
+
+        // cycle through all the pages of the subcategory
+        while (pageLength--) {
+            // store filter length
+            let searchTermSecondaryLength = this.searchTermSecondary.length;
+            // cycle through filters 
+            while (searchTermSecondaryLength--) {
+                // if the filter is match, exit and dont action on page value
+                if (page[pageLength].toLowerCase().indexOf(this.searchTermSecondary[searchTermSecondaryLength].toLowerCase()) >= 0) {
+                    break;
+                } 
+                // if no matches were found, action on page value
+                else if (searchTermSecondaryLength === 0) {
+                    console.log(page[pageLength]);
+                }
+            }
+        }
+    }
+    /**
+     * Function Name:   getPage
+     * Description:     Get the page object of the subcategory it belongs to
+     * @param           {object} category the JSON data for the category/subcategory to access the page data
+     *                  used to perform a deeper search
+     */
+    private getPage(category: object): string[] {
+        return category[this.subcategoryPageKey];
+    }
+
+    /**
+     * Function Name:   getPageLength
+     * Description:     Calculate and return the number of pages in the pages array
+     * @param           {string[]} page contains an array of strings that describe the page name (district names)
+     */
+    private getPageLength(page: string[]): number {
+        return page.length;
     }
 }
 
