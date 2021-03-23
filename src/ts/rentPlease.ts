eab43160df42c794cfe9be4ae3e65d338b7ab9f2
rancy-san <san00003@algonquinlive.com>
2021-03-22 18:57:19 -0700

    Changed function name

--- src/ts/rentPlease.ts
+++ src/ts/rentPlease.ts
@@ -6,22 +6,24 @@
  * 
  */
 
- // Web crawling class
+// Web crawling class
 const CrawlRental = require('./webcrawl/crawlRental');
+const DistrictScan = require('./tools/wiki_scan/districtScan');
 
 export class RentPlease {
     // holds the crawlRental class to webcrawl rental web applications
-    private crawlRental:any;
+    private crawlRental: any;
+    private districtScan: any;
 
     constructor()
-    constructor(arg:string)
+    constructor(arg: string)
     /**
      * Description:     Instantiate classes for use in the Rent, Please! application.
      * @param           {string} arg the processed argument character from the commandline.
      */
-    constructor(arg?:string){
+    constructor(arg?: string) {
         // instantiate web crawler to first obtain rental data pointing to the arg parameter
-       this.crawlRental = new CrawlRental(arg);
+        this.crawlRental = new CrawlRental(arg);
     }
 
     /**
@@ -30,18 +32,24 @@ export class RentPlease {
      *                  set during initializtion of the CrawlRental object 
      *                  in the constructor.
      */
-    public seekRental():void {
+    public seekRental(): void {
         this.crawlRental.crawl();
         return;
     }
 
-    public updateNeighbourhoodList() {
-
+    public updateDistrictList() {
+        this.districtScan = new DistrictScan();
+        this.districtScan.searchByProvince();
+        //this.districtScan.searchPages("Neighbourhoods in Timmins");
+        /*
+        let redisDatabase:any = new RedisDatabase();
+        redisDatabase.rpushQueueData("testKey", "testData");
+        */
     }
 
-    private buildRentalNetwork():void {}
-    private sortRental():void {}
-    private filterRental():void {}
+    private buildRentalNetwork(): void { }
+    private sortRental(): void { }
+    private filterRental(): void { }
 }
 
 module.exports = RentPlease;
